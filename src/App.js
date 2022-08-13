344187c91d1b6974c2c335d59102cab2e8dbb632
syyad <syyadkhan@yahoo.com>
2022-08-13 11:57:10 -0400

    works great but not with mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+/** @jsxImportSource theme-ui */
+import { Button, Typography } from "@mui/material";
+import { useColorMode } from "theme-ui";
+
 function App() {
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
     <div className="App">
       <h1>Theme ui</h1>
+      <button
+        onClick={(e) => {
+          setColorMode(colorMode === "default" ? "green" : "default");
+        }}
+      >
+        Toggle {colorMode === "default" ? "green 🧛‍♀️" : "Light 🌞"}
+      </button>
+      {/* <Typography>help</Typography> */}
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  initialColorModeName: "light",
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#33e",
+    secondary: "white",
+    modes: {
+      dark: {
+        text: "#fff",
+        background: "#000",
+        primary: "#0cf",
+      },
+      green: {
+        text: "#fff",
+        background: "green",
+        primary: "#0cf",
+      },
+    },
+  },
+};
+
+export default theme;
